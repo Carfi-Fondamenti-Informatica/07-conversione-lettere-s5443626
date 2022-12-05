ef6cf5d11fa1bf7e46fe8f4e70dd1d9c2d8e7044
s5443626 <117823998+s5443626@users.noreply.github.com>
2022-12-05 15:20:53 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,7 +1,7 @@
 
 bool funzione (char &a){
-  if (((a >= 65)and (a<=90))or((a>=97)and(a<=122))){
-    if ((a>=65)and (a<=90)){
+  if (((a>=65)and(a<=90))or((a>=97)and(a<=122))){
+    if ((a>=65)and(a<=90)){
       a=(a+32);
     } else if ((a>=97)and(a<=122)){
       a=(a-32);
