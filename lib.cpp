8d2f2d6d7d227329e713e31460085a4764ac85c6
s5443626 <117823998+s5443626@users.noreply.github.com>
2022-12-05 15:14:33 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
 
+bool funzione (char &a){
+  if (((a >= 65)and (a<=90))or((a>=97)and(a<=122))){
+    if ((a>=65)and (a<=90)){
+      a=(a+32);
+    } else if ((a>=97)and(a<=122)){
+      a=(a-32);
+    }return true;
+  }else {
+    return false;
+  }
+}
+    
