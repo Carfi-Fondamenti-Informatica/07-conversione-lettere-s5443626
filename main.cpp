c6cfbf87eee51a95992c8792dbe431ca8f2ccd0a
s5443626 <117823998+s5443626@users.noreply.github.com>
2022-12-05 15:16:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
+#inclide "lib.h"
+using namespace std;
 int main(){
+  char a=0;
+  cin >> a;
+  if (funzione(a)==true){
+    cout << a << endl;
+  }else if (funzione (a) == falso){
+    cout << "errore" << endl;
+  }
   return 0;
 }
